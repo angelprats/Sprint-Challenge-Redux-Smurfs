a58adfe18131c62483a1b0646290d5f5079b895d
angel prats <angelpratz@gmail.com>
2019-05-31 17:12:13 -0400

    cleaned up code

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -14,11 +14,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <h1 className="main-text">SMURFS! 2.0 W/ <span className="redux-span">Redux</span></h1>
         <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-        {/* <Smurf /> */}
         <SmurfList />
         <Form />
       </div>
